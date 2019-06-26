783a95af9b5c2bd7cc6cc6818f261eda579ad798
ender.saka <ender.saka@gmail.com>
2019-06-27 01:11:41 +0200

    Basic implementation of the DataSource responsible for retreiving data from the server.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import DataSource from './DataSource.js';
 
-function App() {
-	new DataSource();
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+window.breeds = null;
+
+function getBreedsCallback(breeds) {
+	console.log(breeds);
+	window.breeds = breeds;
+}
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		let ds = new DataSource();
+		ds.getBreeds(getBreedsCallback);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
+
 }
 
 export default App;
--- src/DataSource.js
+++ src/DataSource.js
@@ -3,6 +3,31 @@ class DataSource {
 	constructor() {
 		console.log('I am DataSource');
 	}
+
+	getBreeds(callback) {
+		window.fetch('https://dog.ceo/api/breeds/list/all')
+		.then(
+			function(response) {
+				if (response.ok) {
+					return response.json();
+				}
+
+				throw new Error('Something went wrong with the response.');
+			}
+		)
+		.then(
+			function(json) {
+				console.log(json.message);
+				callback(json.message);
+				return json.message;
+			}
+		)
+		.catch(
+			function(error) {
+				console.log('window.fetch failed! "', error.message, '"');
+			}
+		);
+	}
 }
 
 export default DataSource;
