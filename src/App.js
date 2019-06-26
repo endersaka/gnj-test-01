f39ee9e2dce0717344ffe00ac431fc20b9709e39
ender.saka <ender.saka@gmail.com>
2019-06-27 01:21:13 +0200

    Added some comments to document previous commit changes.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*----------------------------------------------------------------------------*/
+/* TODO: this code will be probably moved to the component that will be
+ * responsible to render the dog breeds list.
+ */
+
 import DataSource from './DataSource.js';
 
+// Global variable where dog breeds are stored.
 window.breeds = null;
 
+// Call back used to consume dog breeds when they are loaded.
 function getBreedsCallback(breeds) {
 	console.log(breeds);
 	window.breeds = breeds;
 }
+/*----------------------------------------------------------------------------*/
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
+		// Test the above code. Create a DataSource.
 		let ds = new DataSource();
+
+		// Get all the breeds asyncronously and store them into window.breeds
+		// when done.
 		ds.getBreeds(getBreedsCallback);
 	}
 
