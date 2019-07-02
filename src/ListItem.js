96e49ddfe135d79d6b2e5e861465563cdddb325c
ender.saka <ender.saka@gmail.com>
2019-07-02 03:36:17 +0200

    Added some documentation and comments to "src/ListItem.js".

--- src/ListItem.js
+++ src/ListItem.js
@@ -17,23 +17,44 @@ class ListItem extends Component {
 	constructor(props) {
 		super(props);
 
+		// Required if you want to use this as a reference to this Component in
+		// to the binded methods.
 		this.handleMouseOver = this.handleMouseOver.bind(this);
 		this.handleMouseOut = this.handleMouseOut.bind(this);
 
+		// References used to access DOM elements that are children of this
+		// React Component.
 		this.breedItemRef = React.createRef();
 		this.breedBodyRef = React.createRef();
 		this.breedLabelRef = React.createRef();
 
+		// Used by the event handling architecure to keep track of the element
+		// the mouse is leaving.
 		this.currentElement = null;
 	}
 
+	/**
+	 * Adds a class to the class attribute of the element. This implementation
+	 * permits to work without using the classList property (a DOMTokenList
+	 * instance) of the Element class.
+	 *
+	 * @todo Move to a more generic module or extend the Element class with a
+	 * 		 new method that implements this function.
+	 *
+	 * @param {[Element]} element  The HTML Element to modify.
+	 * @param {[string|String]} newClass The new class to add to the class attribute.
+	 */
 	addClassName(element, newClass) {
 		// Validate classToAdd.
 		if (!isString(newClass)) {
 			return;
 		}
+
+		// TODO: further validation required. Suggest to test match.length === 1
+		// and match[0] === newClass. In which case I think we can avoid isString().
+		// Also, this code should be moved in to a function in a utilities module.
 		let match = newClass.match(/[^\d][\w\d-_]*/g);
-		console.log(match);
+		console.log('Testing class name validity; match:', match);
 
 		let classes = element.className.split(' ');
 		if (classes.includes(newClass)) {
@@ -45,31 +66,52 @@ class ListItem extends Component {
 		element.className = classes.join(' ');
 	}
 
+	/**
+	 * Remove a class from the class attribute of the element. This
+	 * implementation permits to work without using the classList property (a
+	 * DOMTokenList instance) of the Element class.
+	 *
+	 * @todo Move to a more generic module or extend the Element class with a
+	 * 		 new method that implements this function.
+	 *
+	 * @param {[Element]} element  The HTML Element to modify.
+	 * @param {[string|String]} classNameToRemove The class to remove from the class attribute.
+	 */
 	removeClassName(element, classNameToRemove) {
 		// Validate classToAdd.
 		if (!isString(classNameToRemove)) {
 			return;
 		}
+
+		// TODO: further validation required. Suggest to test match.length === 1
+		// and match[0] === newClass. In which case I think we can avoid isString().
+		// Also, this code should be moved in to a function in a utilities module.
 		let match = classNameToRemove.match(/[^\d][\w\d-_]*/g);
-		console.log(match);
+		// console.log('Testing class name validity; match:', match);
 
 		let classes = element.className.split(' ');
 		let index = classes.indexOf(classNameToRemove);
-		console.log('index:', index);
+		// console.log('index:', index);
 		if (index === -1) {//!classes.includes(classNameToRemove)) {
 			return;
 		}
-		console.log('classes:', classes);
+		// console.log('classes:', classes);
 
 		classes = classes.splice(index - 1, 1);
-		console.log('classes.splice(index - 1, 1):', classes);
+		// console.log('classes.splice(index - 1, 1):', classes);
 
 		let newClassName = classes.join(' ');
-		console.log('classes.join(\' \'):', newClassName);
+		// console.log('classes.join(\' \'):', newClassName);
 
 		element.className = classes.join(' ');
 	}
 
+	/**
+	 * An event handler that manages the 'mouseover' events triggered by
+	 * descendant DOM elements of this Component.
+	 *
+	 * @param  {SyntheticEvent} e The React SyntheticEvent.
+	 */
 	handleMouseOver(e) {
 		e.stopPropagation();
 		e.preventDefault();
@@ -94,6 +136,12 @@ class ListItem extends Component {
 		this.addClassName(target, 'dog-breeds-bigger');
 	}
 
+	/**
+	 * An event handler to manage the 'mouseout' events triggered by descendant
+	 * DOM elements of this Component.
+	 *
+	 * @param  {SyntheticEvent} e The React SyntheticEvent.
+	 */
 	handleMouseOut(e) {
 		e.stopPropagation();
 		e.preventDefault();
